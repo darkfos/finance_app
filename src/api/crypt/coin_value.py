d7c2e44484e9936fb1437712b8eb270609daffed
darkfos82 <darkfos82@gmail.com>
2024-07-01 17:14:04 +0300

    Added crypt coins

---
+++ src/api/crypt/coin_value.py
@@ -0,0 +1,53 @@
+#System
+from typing import Union, Dict, List
+
+
+#Other
+from requests import Session
+
+
+#Local
+from src.settings.auth_settings import AuthSettings
+
+
+class CoinValue:
+
+    def __init__(self):
+        self.session = Session()
+        self.auth: AuthSettings = AuthSettings()
+
+    def get_currencies(self) -> Union[Dict[Union[str, int], Union[str, int, float, None]], None]:
+        """
+        Получение электронных валют
+        :return:
+        """
+
+        response = self.session.get(
+            url=self.auth.url_course_coin+"currencies",
+            params={
+                "api_key": self.auth.api_coin_value_key
+            }
+        )
+
+        if response.status_code == 200:
+            return response.json()
+
+        return None
+
+    def get_currencies_symbols(self) -> Union[Dict[str, List[Dict[str, int]]], None]:
+        """
+        Получение всех криптовалют и их цеников
+        :return:
+        """
+
+        response = self.get_currencies()
+
+        if response:
+            all_currencies: dict = {}
+            for value in response.get("data"):
+                all_currencies[value.get("name")] = {
+                    "price": value.get("values").get("USD").get("price"),
+                    "symbol": value.get("symbol")
+                }
+            return all_currencies
+        return None
--- src/settings/auth_settings.py
+++ src/settings/auth_settings.py
@@ -9,4 +9,6 @@ class AuthSettings:
 
     salt: str = os.getenv("SALT")
     api_course_value_key: str = os.getenv("API_COURSE_VALUE_KEY")
+    api_coin_value_key: str = os.getenv("API_COIN_KEY")
     url_course_value: str = os.getenv("URL_API_COURSE_VALUE")
+    url_course_coin: str = os.getenv("URL_API_COIN")
